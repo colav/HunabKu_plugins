d26e8b4dc5351171e55b7762ca7ff9c00d237d97
Omar Zapata <Omar.Zapata@cern.ch>
2023-03-03 00:39:33 -0500

    fixed code quality for template plugin

--- HunabKu_template/hunabku_template/endpoints/Hello.py
+++ HunabKu_template/hunabku_template/endpoints/Hello.py
@@ -1,15 +1,15 @@
 from hunabku.HunabkuBase import HunabkuPluginBase, endpoint
 from hunabku.Config import Config, Param
 
+
 class Hello(HunabkuPluginBase):
     config = Config()
     config += Param(myvar="myvalue", doc="this is an example var")
     config.subcategory += Param(port=8080).doc("this is other way to set doc")
-    config.subcategory += Param(host="localhost") #may you want a var without doc
-    
+    config.subcategory += Param(host="localhost")  # may you want a var without doc
+
     def __init__(self, hunabku):
         super().__init__(hunabku)
-
 
     @endpoint('/hello', methods=['GET'])
     def hello(self):
