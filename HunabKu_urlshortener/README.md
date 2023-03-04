2f39e8f07bc0818ac8ac4bd4a8efecf74c8784ce
Omar Zapata <Omar.Zapata@cern.ch>
2023-03-04 03:31:39 -0500

    working in the plugin for url shortener

---
+++ HunabKu_urlshortener/README.md
@@ -0,0 +1,56 @@
+<center><img src="https://raw.githubusercontent.com/colav/colav.github.io/master/img/Logo.png"/></center>
+
+# HunabKu urlshortener plugin 
+This is a urlshortener for xyz project
+replace urlshortener for the name of the plugin everiwhere.
+
+# Description
+Write something meaningful her ;)
+
+# Installation
+
+## Dependencies
+What do I need fot this plugin, it could be external services etc..
+
+## Package
+Write here how to install this plugin
+usauly is 
+
+`pip install hunabku_urlshortener`
+
+or
+
+`pip install hunabku[urlshortener]`
+
+# Usage
+Should I know something is not is apidocs?
+if yes, then put it here.
+
+Additional parameters for hunabku in the config should be here as well.
+example :
+
+```
+# myvar
+# this is an example var
+config.hunabku_urlshortener.Hello.Hello.myvar = "myvalue"
+
+# port
+# this is other way to set doc
+config.hunabku_urlshortener.Hello.Hello.subcategory.port = 8099
+
+# host
+# 
+config.hunabku_urlshortener.Hello.Hello.subcategory.host = "host"
+
+```
+Those parameters are not really needed in the condfig file, it is just for illustration.
+
+
+# License
+BSD-3-Clause License 
+
+# Links
+http://colav.udea.edu.co/
+
+
+
---
+++ HunabKu_urlshortener/setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Copyright (c) Colav.
+# Distributed under the terms of the Modified BSD License.
+
+# -----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython)
+# -----------------------------------------------------------------------------
+
+# See https://stackoverflow.com/a/26737258/2268280
+# sudo pip3 install twine
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+# For test purposes
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+import os
+import sys
+import codecs
+
+
+v = sys.version_info
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+shell = False
+if os.name in ('nt', 'dos'):
+    shell = True
+    warning = "WARNING: Windows is not officially supported"
+    print(warning, file=sys.stderr)
+
+
+def main():
+    setup(
+        # Application name:
+        name="Hunabku_urlshortener",
+
+        # Version number (initial):
+        version=get_version('hunabku_urlshortener/_version.py'),
+
+        # Application author details:
+        author="Colav",
+        author_email="colav@udea.edu.co",
+
+        # Packages
+        packages=find_packages(exclude=['tests']),
+
+        # Include additional files into the package
+        include_package_data=True,
+
+        # Details
+        url="https://github.com/colav/Hunabku_plugins",
+        #
+        license="BSD",
+
+        description="Hunabku plguin",
+
+        long_description=open("README.md").read(),
+
+        long_description_content_type="text/markdown",
+
+        # Dependent packages (distributions)
+        # put you packages here
+        install_requires=[
+            'hunabku',
+            'validators',
+            'pymongo'
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
