6ebafc23c29d1addbcffc092837ccfd88192fff5
Omar Zapata <Omar.Zapata@cern.ch>
2023-03-09 00:43:09 -0500

    added documentaion for plugins

--- HunabKu_template/hunabku_template/endpoints/Hello.py
+++ HunabKu_template/hunabku_template/endpoints/Hello.py
@@ -18,10 +18,8 @@ class Hello(HunabkuPluginBase):
         @apiName Hello
         @apiGroup Template
 
-        @apiParam {Number} id Users unique ID.
 
-        @apiSuccess {String} firstname Firstname of the User.
-        @apiSuccess {String} lastname  Lastname of the User.
+        @apiSuccess {String} {'hello': 'world'}.
         """
         if self.valid_apikey():
             response = self.app.response_class(
