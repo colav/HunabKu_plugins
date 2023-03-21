fa0fe3953b9adf4f3f7ffe273e1f748ecf5873cd
Omar Zapata <Omar.Zapata@cern.ch>
2023-03-21 07:52:29 -0500

    working in generic test that run hunabku to load all the plugins

---
+++ tests/test_generic.py
@@ -0,0 +1,37 @@
+# import subprocess
+import requests
+import sys
+import os
+from shutil import rmtree
+from command import run, CommandException
+
+from hunabku.Hunabku import Hunabku
+from hunabku.Config import ConfigGenerator
+
+import unittest
+
+
+class TestHunabku(unittest.TestCase):
+    """
+    Class to tests hunabku server options
+    """
+
+    def setUp(self):
+        print('running setUp')
+
+    def test__load_plugins(self):
+        print('############################ running load plugins tests ############################')
+        res = run(['hunabku_server', '--config', 'config.py'])
+        print(res.output.decode())
+        if res.exit != 0:
+            print("ERROR: loading plugins")
+            sys.exit(res.exit)
+
+    def tearDown(self):
+        print('############################ running tearDown ############################')
+        if os.path.exists("config.py"):
+            os.remove("config.py")
+
+
+if __name__ == '__main__':
+    unittest.main()
