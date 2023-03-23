b4a1d2d67836222cf187d23acd052c62d4cbebcd
Omar Zapata <Omar.Zapata@cern.ch>
2023-03-22 21:47:26 -0500

    executed flask test with multiprocessing

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,8 +1,10 @@
 # import subprocess
-import requests
 import sys
 import os
 from shutil import rmtree
+from time import sleep
+import importlib
+
 from command import run, CommandException
 
 from hunabku.Hunabku import Hunabku
@@ -10,6 +12,7 @@ from hunabku.Config import ConfigGenerator
 
 import unittest
 
+import multiprocessing
 
 class TestHunabku(unittest.TestCase):
     """
@@ -18,19 +21,36 @@ class TestHunabku(unittest.TestCase):
 
     def setUp(self):
         print('############################ running setUp ############################')
-        res = run(['hunabku_server', '--generate_config', 'config.py','--overwrite'])
+        try:
+            res = run(['hunabku_server', '--generate_config', './config.py','--overwrite'])
+        except CommandException as e:
+            print(f"\33[41m {e.output} \33[0m")
+            sys.exit(e.exit)
         print(res.output.decode())
         if res.exit != 0:
             print("ERROR: in generate_config ")
             sys.exit(res.exit)
+        loader = importlib.machinery.SourceFileLoader('config', './config.py')
+        spec = importlib.util.spec_from_loader(loader.name, loader)
+        config = importlib.util.module_from_spec(spec)
+        loader.exec_module(config)
+        self.config = config.config
+
 
     def test__load_plugins(self):
         print('############################ running load plugins tests ############################')
-        res = run(['hunabku_server', '--config', 'config.py'])
-        print(res.output.decode())
-        if res.exit != 0:
-            print("ERROR: loading plugins")
-            sys.exit(res.exit)
+        def hunabku():
+            self.config.use_reloader = False
+            server = Hunabku(self.config)
+            server.apidoc_setup()
+            server.load_plugins()
+            server.generate_doc()
+            server.start()
+        proc = multiprocessing.Process(target=hunabku, args=())
+        proc.start()
+        # Terminate the process
+        sleep(5)  # Corre el proceso de Flask durante 5 segundos
+        proc.terminate()
 
     def tearDown(self):
         print('############################ running tearDown ############################')
