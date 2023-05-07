93433d26edb9b034a2ba607558c8aa933a74ef5f
Omar Zapata <Omar.Zapata@cern.ch>
2023-05-07 13:27:27 -0500

    SIIU: added search by group name using elastic

--- HunabKu_siiu/hunabku_siiu/endpoints/SIIU.py
+++ HunabKu_siiu/hunabku_siiu/endpoints/SIIU.py
@@ -42,7 +42,8 @@ class SIIU(HunabkuPluginBase):
         @apiParam {String} apikey  Credential for authentication
         @apiParam {String} search  keyword for text search.
         @apiParam {String} CODIGO  project id.
-        @apiParam {String} GRUPO_CODIGO  Colciencias Group ID ex:"COL0008423"
+        @apiParam {String} group_code  Colciencias Group ID ex:"COL0008423"
+        @apiParam {String} group_name  name of the research group (returns the projects for this group)
 
         @apiSuccess {Object}  Resgisters from MongoDB in Json format.
 
@@ -55,15 +56,17 @@ class SIIU(HunabkuPluginBase):
             # An specific product
             curl -i http://apis.colav.co/siiu/project?apikey=XXXX&CODIGO=2013-86
             # An projects given a group id
-            curl -i http://apis.colav.co/siiu/project?apikey=XXXX&GRUPO_CODIGO=COL0008423
+            curl -i http://apis.colav.co/siiu/project?apikey=XXXX&group_code=COL0008423
+            # An projects given a group name
+            curl -i http://apis.colav.co/siiu/project?apikey=XXXX&group_name="psicologia cognitiva"
 
         """
         if self.valid_apikey():
 
             keyword = self.request.args.get('search')
             codigo = self.request.args.get('CODIGO')
-            grp_codigo = self.request.args.get('GRUPO_CODIGO')
-
+            grp_codigo = self.request.args.get('group_code')
+            group_name = self.request.args.get('group_name')
             if keyword:
                 if not self.es.indices.exists(index=self.config.es_project_index):
                     response = self.app.response_class(
@@ -120,6 +123,46 @@ class SIIU(HunabkuPluginBase):
                     status=200,
                     mimetype='application/json'
                 )
+                return response
+
+            if group_name:
+                if not self.es.indices.exists(index=self.config.es_project_index):
+                    response = self.app.response_class(
+                        response=self.json.dumps(
+                            {"msg": f"Internal error, index {self.config.es_project_index} not found in Elastic Search"}),
+                        status=500,
+                        mimetype='application/json'
+                    )
+                    return response
+                body = {
+                    "query": {
+                        "bool": {
+                            "must": [
+                                {"match_phrase": {
+                                    "project_participant.group.NOMBRE_COMPLETO":  group_name}},
+                            ]
+                        }
+                    }
+                }
+
+                # get the start time
+                st = time.time()
+                s = Search(using=self.es, index=self.config.es_project_index)
+                s = s.update_from_dict(body)
+                s = s.extra(track_total_hits=True)
+                s.execute()
+                data = [hit.to_dict() for hit in s.scan()]
+                response = self.app.response_class(
+                    response=self.json.dumps(data),
+                    status=200,
+                    mimetype='application/json'
+                )
+                # get the end time
+                et = time.time()
+                # get the execution time
+                elapsed_time = et - st
+                print(f'Search for "{group_name}" Execution time:',
+                      elapsed_time, 'seconds')
                 return response
 
             data = {
